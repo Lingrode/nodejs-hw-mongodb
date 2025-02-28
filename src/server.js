756fea62befc9c26d237c05ca28445917b6849dc
Illia Hrymalo <grimaloilya@gmail.com>
2025-02-27 22:10:59 -0700

    created server

---
+++ src/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { getEnvVar } from './utils/getEnvVar.js';
+
+const PORT = Number(getEnvVar('PORT', '3000'));
+
+export const startServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(pino({ transport: { target: 'pino-pretty' } }));
+
+  app.use('*', (req, res) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+};
