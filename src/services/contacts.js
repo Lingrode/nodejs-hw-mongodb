35b2dcecd3afce654dc411d10154bdc1bfbb81e6
Illia Hrymalo <grimaloilya@gmail.com>
2025-03-07 19:01:08 -0700

    creating contact

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -9,3 +9,8 @@ export const getContactById = async (id) => {
   const contact = await ContactsCollection.findById(id);
   return contact;
 };
+
+export const createContact = async (contactData) => {
+  const contact = await ContactsCollection.create(contactData);
+  return contact;
+};
