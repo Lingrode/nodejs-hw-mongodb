4a62239b2f4309575d4494257a317c70d436a699
Illia Hrymalo <grimaloilya@gmail.com>
2025-03-14 23:51:34 -0600

    sorting queries

---
+++ src/constants/index.js
@@ -0,0 +1,15 @@
+export const SORT_ORDER = {
+  ASC: 'asc',
+  DESC: 'desc',
+};
+
+export const SORT_FIELDS = [
+  '_id',
+  'name',
+  'phoneNumber',
+  'email',
+  'isFavourite',
+  'contactType',
+  'createdAt',
+  'updatedAt',
+];
---
+++ src/utils/parseSortParams.js
@@ -0,0 +1,20 @@
+import { SORT_FIELDS, SORT_ORDER } from '../constants/index.js';
+
+const parseSortOrder = (sortOrder) => {
+  return [SORT_ORDER.ASC, SORT_ORDER.DESC].includes(sortOrder)
+    ? sortOrder
+    : SORT_ORDER.ASC;
+};
+
+const parseSortBy = (sortBy) => {
+  return SORT_FIELDS.includes(sortBy) ? sortBy : 'name';
+};
+
+export const parseSortParams = (query) => {
+  const { sortOrder, sortBy } = query;
+
+  const parsedSortOrder = parseSortOrder(sortOrder);
+  const parsedSortBy = parseSortBy(sortBy);
+
+  return { sortOrder: parsedSortOrder, sortBy: parsedSortBy };
+};
