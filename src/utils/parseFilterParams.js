f3dec09a96a53fd695dbae75d7d44eeafdbe048b
Illia Hrymalo <grimaloilya@gmail.com>
2025-03-15 01:10:42 -0600

    filtering

---
+++ src/utils/parseFilterParams.js
@@ -0,0 +1,25 @@
+const parseContactType = (type) => {
+  const isString = typeof type === 'string';
+  if (!isString) return;
+
+  const allowedTypes = ['work', 'home', 'personal'];
+  if (allowedTypes.includes(type)) return type;
+};
+
+const parseIsFavourite = (value) => {
+  if (value === 'true') return true;
+  if (value === 'false') return false;
+  return undefined;
+};
+
+export const parseFilterParams = (query) => {
+  const { type, isFavourite } = query;
+
+  const parsedType = parseContactType(type);
+  const parsedIsFavourite = parseIsFavourite(isFavourite);
+
+  return {
+    type: parsedType,
+    isFavourite: parsedIsFavourite,
+  };
+};
