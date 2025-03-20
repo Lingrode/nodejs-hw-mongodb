786c6ee5651cefec65fa77e96f1a260eb7049723
Illia Hrymalo <grimaloilya@gmail.com>
2025-03-20 15:56:26 -0600

    created user and session models

---
+++ src/db/models/session.js
@@ -0,0 +1,29 @@
+import { model, Schema } from 'mongoose';
+
+const sessionSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    accessToken: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+      required: true,
+    },
+    accessTokenValidUntil: {
+      type: String,
+      required: true,
+    },
+    refreshTokenValidUntil: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true, versionKey: false },
+);
+
+export const SessionCollection = model('session', sessionSchema);
