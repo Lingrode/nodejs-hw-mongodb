c148d2d779ba3c7c0485156dd9a6fb78cffedbfc
Illia Hrymalo <grimaloilya@gmail.com>
2025-04-04 19:48:59 -0600

    docs for route /auth/login

--- src/services/auth.js
+++ src/services/auth.js
@@ -45,7 +45,7 @@ const createSession = () => {
 export const loginUser = async (payload) => {
   const user = await UserCollection.findOne({ email: payload.email });
 
-  if (!user) throw createHttpError(404, 'User not found');
+  if (!user) throw createHttpError(401, 'User not found');
 
   const isEqual = await bcrypt.compare(payload.password, user.password);
 
